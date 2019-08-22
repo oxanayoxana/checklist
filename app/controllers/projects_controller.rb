01ca1668be60cc3b1b1bb85aa262cdd71130c063
Oxana Oxana <oksana.oleshchenko@gmail.com>
2019-08-22 15:32:11 +0300

    view to fill in project and select checklist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,26 @@ class ProjectsController < ApplicationController
   end
 
   def show; end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save!
+      redirect_to projects_path,
+                  flash: { notice: 'Project was successfully created.' }
+    else
+      redirect_to new_project_path,
+                  flash: { error: @project.errors.full_messages.to_sentence }
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
