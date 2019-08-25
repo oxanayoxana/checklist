1052c9772e9985bf79dea37ac0aeef5d95fbc7c7
Oxana Oxana <oksana.oleshchenko@gmail.com>
2019-08-25 08:59:57 +0300

    add possibility to create project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,27 +3,18 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
-    # @checklists = Checklist.all
   end
 
   def show; end
 
   def new
     @project = Project.new
-    # @checklists = Checklist.all
-    @checklist = Checklist.find(params[:id])
   end
 
   def create
     @project = Project.new(project_params)
-    # @checklist = Checklist.find(params[:id])
-    # @checklist = Checklist.find(params[:checklist_id])
-    # @questions = @checklist.questions.all
-    # @project = @checklist.projects.build(project_params)
-    @checklist = @project.checklist
 
     if @project.save!
-      # project = Project.find(params[:id])
       @checklist = @project.checklist
 
       redirect_to new_checklist_questions_answer_path(@checklist.id),
